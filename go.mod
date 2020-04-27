9b89b20b4f1ed39c38bdf250bfeafce08ff40ac8
Kieron Browne <kbrowne@pivotal.io>
2020-04-27 18:22:50 +0100

    global logger

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/kieron-pivotal/lager-o-clock
 
 go 1.14
+
+require (
+	code.cloudfoundry.org/lager v2.0.0+incompatible
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 // main shows a lager logging example
 package main
 
+import (
+	"os"
+
+	"code.cloudfoundry.org/lager"
+)
+
+var logger lager.Logger
+
 type objA struct {
 	b *objB
 }
@@ -12,6 +20,7 @@ func newA(b *objB) *objA {
 }
 
 func (a *objA) doItA() {
+	logger.Debug("obj-a.do-it-a")
 	a.b.doItB()
 }
 
@@ -26,6 +35,7 @@ func newB(c *objC) *objB {
 }
 
 func (b *objB) doItB() {
+	logger.Debug("obj-b.do-it-b")
 	b.c.doItC()
 }
 
@@ -36,10 +46,13 @@ func newC() *objC {
 }
 
 func (c *objC) doItC() {
-
+	logger.Debug("obj-c.do-it-c")
 }
 
 func main() {
+	logger = lager.NewLogger("logging-example")
+	logger.RegisterSink(lager.NewPrettySink(os.Stderr, lager.DEBUG))
+
 	c := newC()
 	b := newB(c)
 	a := newA(b)
